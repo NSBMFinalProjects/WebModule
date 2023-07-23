a6732f3b494b3c46081bac6b33efd59ec96dff4b
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-23 12:50:24 +0530

    Added the db schema for the users table

---
+++ schema/schema.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  ID uuid DEFAULT uuid_generate_v4(),
+  username VARCHAR(25) NOT NULL UNIQUE,
+  display_name VARCHAR(35) NOT NULL,
+  photo_url VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  provider VARCHAR(50) NOT NULL,
+  provider_id INT NOT NULL,
+  PRIMARY KEY(ID)
+);
