ddadf14ed93bb713f0db91fcd197b40452888959
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-23 21:43:45 +0530

    Made the email feild not unique

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
   username VARCHAR(25) NOT NULL UNIQUE,
   display_name VARCHAR(35) NOT NULL,
   photo_url VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) UNIQUE,
   provider VARCHAR(50) NOT NULL,
   provider_id INT NOT NULL,
   PRIMARY KEY(ID)
