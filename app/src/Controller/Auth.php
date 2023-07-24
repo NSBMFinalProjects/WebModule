91182d91d819b17243cf63336a8d69015775a0c7
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-24 17:44:33 +0530

    Added the ability to fetch the user data from  the session

--- app/src/Controller/Auth.php
+++ app/src/Controller/Auth.php
@@ -2,6 +2,9 @@
 namespace App\Controller;
 
 use App\Middleware\Auth as AppAuth;
+use App\Models\User;
+use App\Utils\Token;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +13,67 @@ use Symfony\Component\Routing\Annotation\Route;
 class Auth extends AbstractController
 {
     #[Route(methods: ['GET'])]
+    public function me(Request $req): Response
+    {
+        if (!$req->isMethod(Request::METHOD_GET)) {
+            return new Response(
+                json_encode([]),
+                Response::HTTP_METHOD_NOT_ALLOWED,
+                [
+                'content-type' => 'application/json'
+                ]
+            );
+        }
+
+        if (!AppAuth::isAuthed()) {
+            return new Response(
+                json_encode([]),
+                Response::HTTP_UNAUTHORIZED,
+                [
+                'content-type' => 'application/json'
+                ]
+            );
+        }
+
+        try {
+            Token::decode($_COOKIE['session']);
+
+            $user = new User;
+            $user->getUser(username: Token::$sub);
+
+            return new Response(
+                json_encode(
+                    array(
+                    'id' => $user->getID(),
+                    'username' => $user->getUsername(),
+                    'photo_url' => $user->getPhotoURL(),
+                    'display_name' => $user->getDisplayName(),
+                    'email' => $user->getEmail()
+                    )
+                ),
+                Response::HTTP_OK,
+                [
+                'content-type' => 'application/json'
+                ]
+            );
+        } catch (Exception $e) {
+            return new Response(
+                json_encode([]),
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                [
+                'content-type' => 'application/json'
+                ]
+            );
+        }
+    }
+  
+    #[Route(methods: ['GET'])]
     public function logout(Request $req): Response
     {
         if (!$req->isMethod(Request::METHOD_GET)) {
             return new Response(
                 'method not allowed',
-                Response::HTTP_BAD_REQUEST,
+                Response::HTTP_METHOD_NOT_ALLOWED,
                 [
                 'content-type' => 'application/json'
                 ]
