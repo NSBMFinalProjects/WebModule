99b39a9a2cea8ff9a2823e2c1f435c66e9dfaeac
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-27 00:29:45 +0530

    Added a background route to fetch the new question
    This route will be called from a cron service at the time when the question time period ends and will  show the new question

--- app/public/routes/question.php
+++ app/public/routes/question.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Models\Question;
+
+$question = new Question();
+try {
+    $question->fetchTodaysQuestion();
+    echo $question->getQuestion();
+} catch(Exception $e)  {
+    echo "No questions to show";
+}
+
 echo "question page";
