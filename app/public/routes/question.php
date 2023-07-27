e296b812ad3b658082466b34436e694922e7d5c5
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-27 09:19:55 +0530

    Added the ability to display the question and the answers

--- app/public/routes/question.php
+++ app/public/routes/question.php
@@ -1,13 +1,49 @@
 <?php
 
+use App\Connnections\RedisDB;
 use App\Models\Question;
 
+$redis = RedisDB::connect();
 $question = new Question();
-try {
-    $question->fetchTodaysQuestion();
-    echo $question->getQuestion();
-} catch(Exception $e)  {
-    echo "No questions to show";
+
+$today = $redis->get('today');
+if ($today != "") {
+    try {
+        $question->markQuestionAsDisplayed($today);
+    } catch (Exception $e) {
+        echo "Something went wrong";
+        return;
+    }
 }
 
-echo "question page";
+try {
+    $question->fetchTodaysQuestion();
+} catch (Exception $e) {
+    echo "Something went wrong";
+    return;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question page</title>
+</head>
+<body>
+    <h1>
+        <?php echo $question->getQuestion() ?>
+    </h1>
+
+    <ul>
+        <?php
+        foreach ($question->getAnswers() as $answer) {
+            echo '<li>' . $answer . '</li>';
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
