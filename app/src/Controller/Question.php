65b4ee2fdfd5f9c0881b3d7a259ad427e261eb8b
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-28 21:50:36 +0530

    Added the ability to answer the question

--- app/src/Controller/Question.php
+++ app/src/Controller/Question.php
@@ -1,8 +1,14 @@
 <?php
 namespace App\Controller;
 
+use App\Connnections\DB;
+use App\Connnections\RedisDB;
+use App\Middleware\Auth;
 use App\Models\Question as AppQuestion;
+use App\Models\User;
+use App\Utils\Token;
 use Exception;
+use Fuel\Validation\Validator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -55,6 +61,122 @@ class Question extends AbstractController
             [
               'content-type' => 'application/json'
             ]
+        );
+    }
+
+    #[Route(name: 'answer-question')]
+    public function answer(Request $req): Response
+    {
+        $now = time();
+
+        if (!$req->isMethod(Request::METHOD_POST)) {
+            return new Response(
+                'method not allowed',
+                Response::HTTP_METHOD_NOT_ALLOWED,
+                [
+                'content-type' => 'application/json' 
+                ]
+            );
+        }
+
+        if (!Auth::isAuthed()) {
+            return new Response(
+                'unauthorized',
+                Response::HTTP_UNAUTHORIZED,
+                [
+                'content-type' => 'application/json'
+                ]
+            );
+        }
+
+        $body = json_decode($req->getContent(), true);
+
+        $v = new Validator;
+        $v->addField('answer', 'answer')->required()->number();
+        $result = $v->run($body);
+        if (!$result->isValid()) {
+            return new Response(
+                'bad Request',
+                Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'application/json']
+            );
+        }
+
+        try {
+          
+            $redis = RedisDB::connect();
+            $db = DB::db();
+
+            $today = $redis->get("today");
+            if ($today == "") {
+                return new Response(
+                    'bad request',
+                    Response::HTTP_BAD_REQUEST,
+                    ['content-type' => 'application/json']
+                );
+            }
+
+            Token::decode($_COOKIE['session']);
+
+            $user = new User;
+            $question = new AppQuestion;
+
+            $user->fetchUser(username: Token::$sub);
+            $question->fetchQuestion($today);
+
+            $stmt = $db->prepare("SELECT id FROM answers WHERE user_id=:user_id AND question_id=:question_id");
+            $stmt->execute(
+                [
+                ':user_id' => $user->getID(),
+                ':question_id' => $today
+                ]
+            );
+            $result = $stmt->fetch();
+            if ($result) {
+                return new Response(
+                    'already_answered',
+                    Response::HTTP_BAD_REQUEST,
+                    ['content-type' => 'application/json']
+                );
+            }
+
+            $isCorrect = false;
+            if ($question->getCorectAnswer() == $body['answer']) {
+                $isCorrect = true;
+            }
+            $delay = $now - $question->getDisplayedDate();
+            if ($delay <= 0 || $delay > 86400) {
+                return new Response(
+                    'timeout',
+                    Response::HTTP_BAD_REQUEST,
+                    ['content-type' => 'application/json']
+                );
+            }
+
+            $stmt = $db->prepare("INSERT INTO answers (user_id, question_id, answer, answer_delay, is_correct) VALUES (:user_id, :question_id, :answer, :answer_delay, :is_correct)");
+            $stmt->execute(
+                [
+                ':user_id' => $user->getID(),
+                ':question_id' => $today,
+                ':answer' => $body['answer'],
+                ':answer_delay' => $delay,
+                ':is_correct' => $isCorrect,
+                ]
+            );
+            $stmt->fetch();
+
+        } catch(Exception $e) {
+            return new Response(
+                $e->getMessage(),
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+                ['content-type' => 'application/json']
+            );
+        }
+
+        return new Response(
+            json_encode(['is_correct' => $isCorrect ]),
+            Response::HTTP_OK,
+            ['content-type' => 'application/json']
         );
     }
 }
