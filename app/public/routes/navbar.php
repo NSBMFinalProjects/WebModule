ba31d60317051b3111220fb00a672b988344c018
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-07-31 20:56:00 +0530

    Added the navbar

---
+++ app/public/routes/navbar.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<div id="navbar">
+  <a href="Qustion page.html" id="logo">Logo</a>
+  <div id="navbar-right">
+    <a class="active" href="index.html">Home</a>
+    <a href="Qustion page.html">Question page</a>
+    <a href="pastquestions.html">Past Question</a>
+    <a href="Leadboard.html">Leadboard</a>
+    <a href="About us.html">Aboutus</a>
+    <a href="contactUs.html">Contact us</a>
+    <a href="Admin page.html">Admin</a>
+  </div>
+</div>
